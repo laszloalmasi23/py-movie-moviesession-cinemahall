0e7e53ac3ba5a1c654e1f308e66eecda6dfe9f80
laszloalmasi23 <laszlo.almasi7@gmail.com>
2026-03-03 20:50:04 +0100

    'Solution'

--- services/movie.py
+++ services/movie.py
@@ -2,7 +2,7 @@ from db.models import Movie
 from django.db.models import QuerySet
 
 
-def get_movies(genres_ids: int = None, actors_ids: int = None) -> QuerySet[Movie]:
+def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None) -> QuerySet[Movie]:
     if not genres_ids and not actors_ids:
         return Movie.objects.all()
 
